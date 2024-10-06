c75fcde162c234eeb90f460f786ca4b33244b30f
Francisca Paredes <francisca.paredes.pr@gmail.com>
2024-10-06 11:31:15 -0300

    update template

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Usuario
 
-# Create your views here.
-def usuariolist(request):    # Recibimos un objeto de tipo request por parte del cliente
-    return render(request, 'usuariolist.html')
+# Create your views here.     # Recibimos un objeto de tipo request por parte del cliente
+def usuariolist(request):
+    # Creamos una variable get_usuaruio le indicamos que haga la consulta a la clase modelo Usuario
+    get_usuarios = Usuario.objects.all()    # Trae toda la data de esta tabla Usuario
+
+    # Creamos una diccionario que represente la data
+    data = {
+        'get_usuarios': get_usuarios
+    }
+    
+    return render(request, 'usuariolist.html', data)    # con data como parametro pedimos que envie esta data (diccionario a usuariolist.html)
